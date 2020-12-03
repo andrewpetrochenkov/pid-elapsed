055c54f88c6d695073c45f90c0173aedf46c2c6b
andrewp-as-is <russianidiot.github@gmail.com>
2020-12-04 00:11:42 +0300

    2020:12:04 00:11

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='pid-elapsed',
+    version='2020.12.3',
     install_requires=open('requirements.txt').read().splitlines(),
     packages=setuptools.find_packages(),
     scripts=['scripts/pid-elapsed']
